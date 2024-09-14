f1b115a044349a6493bd0a85cb3b4621610473b8
Delina <delinayemane2004@gmail.com>
2024-09-14 13:56:48 -0700

    Add project files.

---
+++ HW8/HW8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x = 10;
+    double y = 3; //you can left either one as int it will still get double result
+     double z = x / y;
+     cout << z;
+    return 0;
+}
+
